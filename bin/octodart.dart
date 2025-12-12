22a925eee4ff85fff553b939dec456bf167b2e40
Miguel Martin <miguelmr94@gmail.com>
2025-12-12 19:48:53 +0000

    Add console screen clearing functionality on TUI start

--- bin/octodart.dart
+++ bin/octodart.dart
@@ -1,12 +1,16 @@
 import 'package:commander_ui/commander_ui.dart';
 import 'package:octodart/modules/config/data/config_repository.dart';
 import 'package:octodart/modules/config/domain/config.dart';
+import 'package:octodart/utils/console_clear.dart';
 import 'package:octodart/views/home/home.dart';
 
 Future<void> main() async {
   // 1. Initialize Commander in main
   final commander = Commander(level: Level.verbose);
   final AppConfig config = await loadConfig();
+
+  // Clear screen when TUI starts
+  clearScreen();
 
   // Your application logic can now use these typed fields:
   // SomeGitHubClient client = SomeGitHubClient(token: config.github.authToken);
